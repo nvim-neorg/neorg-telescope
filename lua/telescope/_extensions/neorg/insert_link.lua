d8232b49103524cd864cdc75af4d92366a3d2b76
Daniel Mathiot <d.danymat@gmail.com>
2021-09-18 12:04:02 +0200

    Add support for links in current workspace
    This new feature will search for all neorg files in current neorg workspace and will find all possible links for it.
    When inserted, the link that point to an other file will be added
    [link](:file:#link).
    
    Current limitation: Insert link picker does not show is the link is from
    an other file or not

--- lua/telescope/_extensions/neorg/insert_link.lua
+++ lua/telescope/_extensions/neorg/insert_link.lua
@@ -8,67 +8,155 @@ local neorg_loaded, _ = pcall(require, "neorg.modules")
 
 assert(neorg_loaded, "Neorg is not loaded - please make sure to load Neorg first")
 
-local function get_linkables()
-	local ret = {}
+--- Get a list of all norg files in current workspace
+--- @return table
+local function get_norg_files()
+    local dirman = neorg.modules.get_module("core.norg.dirman")
 
-	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+    if not dirman then
+        return nil
+    end
 
-	for i, line in ipairs(lines) do
-		local heading = { line:match("^%s*(%*+%s+(.+))$") }
-		if not vim.tbl_isempty(heading) then
-			table.insert(ret, { line = i, linkable = heading[2], display = heading[1] })
-		end
+    local current_workspace = dirman.get_current_workspace()
 
-		local marker_or_drawer = { line:match("^%s*(%|%|?%s+(.+))$") }
-		if not vim.tbl_isempty(marker_or_drawer) then
-			table.insert(ret, { line = i, linkable = marker_or_drawer[2], display = marker_or_drawer[1] })
-		end
-	end
+    local norg_files = dirman.get_norg_files(current_workspace[1])
+    norg_files = vim.tbl_map(function (f) return  current_workspace[2] .. "/" .. f end, norg_files)
+    return norg_files
+end
 
-	return ret
+--- Creates links for a `file` specified by `bufnr`
+--- @param bufnr number
+--- @param file string
+--- @return table
+local function get_linkables(bufnr, file)
+    local ret = {}
+
+    local dirman = neorg.modules.get_module("core.norg.dirman")
+
+    if not dirman then
+        return nil
+    end
+
+    local current_workspace = dirman.get_current_workspace()
+
+    if file then
+        file = file:gsub(current_workspace[2], "")
+    end
+
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
+
+    for i, line in ipairs(lines) do
+        local heading = { line:match("^%s*(%*+%s+(.+))$") }
+        if not vim.tbl_isempty(heading) then
+            table.insert(ret, { line = i, linkable = heading[2], display = heading[1], file = file })
+        end
+
+        local marker_or_drawer = { line:match("^%s*(%|%|?%s+(.+))$") }
+        if not vim.tbl_isempty(marker_or_drawer) then
+            table.insert(ret, { line = i, linkable = marker_or_drawer[2], display = marker_or_drawer[1], file = file })
+        end
+    end
+
+    return ret
+end
+
+--- Generate links for telescope
+--- @return table
+local function generate_links()
+    local res = {}
+    local dirman = neorg.modules.get_module("core.norg.dirman")
+
+    if not dirman then
+        return nil
+    end
+
+    local files = get_norg_files()
+
+    if not files then
+        return
+    end
+
+    for _, file in pairs(files) do
+        local bufnr = dirman.get_file_bufnr(file)
+        if not bufnr then
+            return
+        end
+
+        vim.fn.bufload(file)
+
+        -- Because we do not want file name to appear in a link to the same file
+        local file_inserted = (function ()
+            if vim.api.nvim_get_current_buf() == bufnr then
+                return nil
+            else
+                return file
+            end
+        end)()
+
+        local links = get_linkables(bufnr, file_inserted)
+
+        if vim.api.nvim_get_current_buf() ~= bufnr then
+            vim.cmd('bunload ' .. bufnr)
+        end
+
+        vim.list_extend(res, links)
+    end
+
+    return res
 end
 
 return function(opts)
-	opts = opts or {}
+    opts = opts or {}
 
-	pickers.new(opts, {
-		prompt_title = "Insert Link",
-		results_title = "Linkables",
-		finder = finders.new_table({
-			results = get_linkables(),
-			entry_maker = function(entry)
-				return {
-					value = entry.line,
-					display = entry.display,
-					ordinal = entry.linkable,
-					lnum = entry.line,
-				}
-			end,
-		}),
-		-- I couldn't get syntax highlight to work with this :(
-		previewer = nil,
-		sorter = conf.generic_sorter(opts),
-		attach_mappings = function(prompt_bufnr)
-			actions_set.select:replace(function()
-				local entry = actions.get_selected_entry()
-				actions.close(prompt_bufnr)
-				vim.api.nvim_put(
-					{
-						"["
-							.. entry.ordinal:gsub(":$", "")
-							.. "]"
-							.. "("
-							.. entry.display:gsub("^(%W+)%s+.+", "%1")
-							.. entry.ordinal:gsub("[%*#%|_]", "\\%1")
-							.. ")",
-					},
-					"c",
-					false,
-					true
-				)
-				vim.api.nvim_feedkeys("f)a", "t", false)
-			end)
-			return true
-		end,
-	}):find()
+    pickers.new(opts, {
+        prompt_title = "Insert Link",
+        results_title = "Linkables",
+        finder = finders.new_table({
+            results = generate_links(),
+            entry_maker = function(entry)
+                return {
+                    value = entry.line,
+                    display = entry.display,
+                    ordinal = entry.linkable,
+                    lnum = entry.line,
+                    file = entry.file
+                }
+            end,
+        }),
+        -- I couldn't get syntax highlight to work with this :(
+        previewer = nil,
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr)
+            actions_set.select:replace(function()
+                local entry = actions.get_selected_entry()
+                actions.close(prompt_bufnr)
+
+                local inserted_file = (function ()
+                    if entry.file then
+                        return ":" .. entry.file .. ":"
+                    else
+                        return ""
+                    end
+                end)()
+
+                vim.api.nvim_put(
+                    {
+                        "["
+                            .. entry.ordinal:gsub(":$", "")
+                            .. "]"
+                            .. "("
+                            .. inserted_file
+                            .. entry.display:gsub("^(%W+)%s+.+", "%1")
+                            .. entry.ordinal:gsub("[%*#%|_]", "\\%1")
+                            .. ")",
+                    },
+                    "c",
+                    false,
+                    true
+                )
+                vim.api.nvim_feedkeys("f)a", "t", false)
+            end)
+            return true
+        end,
+    }):find()
 end
