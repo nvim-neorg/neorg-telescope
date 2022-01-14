0170e657a9683791810f7090920c84006107f34a
max397574 <max.heinzer397@gmx.ch>
2022-01-14 20:27:43 +0100

    chore: autoformat with stylua

--- lua/telescope/_extensions/neorg/insert_link.lua
+++ lua/telescope/_extensions/neorg/insert_link.lua
@@ -1,10 +1,10 @@
 local actions = require("telescope.actions")
 local actions_set = require("telescope.actions.set")
-local state = require('telescope.actions.state')
+local state = require("telescope.actions.state")
 local finders = require("telescope.finders")
 local pickers = require("telescope.pickers")
 local conf = require("telescope.config").values
-local entry_display = require('telescope.pickers.entry_display')
+local entry_display = require("telescope.pickers.entry_display")
 
 local neorg_loaded, _ = pcall(require, "neorg.modules")
 
@@ -40,7 +40,6 @@ local function get_linkables(bufnr, file)
     else
         lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
     end
-
 
     for i, line in ipairs(lines) do
         local heading = { line:match("^%s*(%*+%s+(.+))$") }
@@ -81,7 +80,7 @@ local function generate_links()
         end
 
         -- Because we do not want file name to appear in a link to the same file
-        local file_inserted = (function ()
+        local file_inserted = (function()
             if vim.api.nvim_get_current_buf() == bufnr then
                 return nil
             else
@@ -101,35 +100,41 @@ return function(opts)
     opts = opts or {}
     local links = generate_links()
 
-
     pickers.new(opts, {
         prompt_title = "Insert Link",
         results_title = "Linkables",
         finder = finders.new_table({
             results = links,
             entry_maker = function(entry)
-                local displayer = entry_display.create {
+                local displayer = entry_display.create({
                     separator = ": ",
                     items = {
                         { width = 30 },
                         { remaining = true },
                     },
-                }
+                })
                 local function make_display(ent)
-                    return displayer {
-                        { ent.file:sub(-30,-1), "NeorgLinkFile"},
-                        { ent.ordinal, "NeorgLinkText"}
-                    }
+                    if entry.file then
+                        return displayer({
+                            { ent.file:sub(-30, -1), "NeorgLinkFile" },
+                            { ent.ordinal, "NeorgLinkText" },
+                        })
+                    else
+                        return displayer({
+                            { ent.ordinal, "NeorgLinkText" },
+                        })
+                    end
                 end
-                if not entry.file then
-                    entry.file = vim.fn.expand("%")
-                end
+                -- if not entry.file then
+                -- entry.file = vim.fn.expand("%:r")
+                -- end
                 return {
                     value = entry.line,
                     display = make_display,
                     ordinal = entry.display,
                     lnum = entry.line,
-                    file = entry.file
+                    file = entry.file,
+                    linkable = entry.linkable,
                 }
             end,
         }),
@@ -141,7 +146,7 @@ return function(opts)
                 local entry = state.get_selected_entry()
                 actions.close(prompt_bufnr)
 
-                local inserted_file = (function ()
+                local inserted_file = (function()
                     if entry.file then
                         -- entry.display = string.gsub(entry.display, entry.file..": ", "")
                         return ":" .. entry.file .. ":"
@@ -150,23 +155,21 @@ return function(opts)
                     end
                 end)()
 
-                print("entry:")
-                dump(entry)
-                vim.api.nvim_put(
-                    {
-                        "{"
-                            .. inserted_file
-                            .. entry.display:gsub("^(%W+)%s+.+", "%1 ")
-                            .. entry.ordinal:gsub("[%*#%|_]", "\\%1")
-                            .. "}"
-                            .. "["
-                            .. entry.ordinal:gsub(":$", "")
-                            .. "]",
-                    },
-                    "c",
-                    false,
-                    true
-                )
+                if inserted_file ~= "" then
+                    vim.api.nvim_put({
+                        "{" .. inserted_file .. entry.ordinal:gsub("^(%W+)%s+.+", "%1 ") .. entry.linkable:gsub(
+                            "[%*#%|_]",
+                            "\\%1"
+                        ) .. "}" .. "[" .. entry.linkable:gsub(":$", "") .. "]",
+                    }, "c", false, true)
+                else
+                    vim.api.nvim_put({
+                        "{" .. inserted_file .. entry.ordinal:gsub("^(%W+)%s+.+", "%1 ") .. entry.linkable:gsub(
+                            "[%*#%|_]",
+                            "\\%1"
+                        ) .. "}",
+                    }, "c", false, true)
+                end
                 vim.api.nvim_feedkeys("hf]a", "t", false)
             end)
             return true
