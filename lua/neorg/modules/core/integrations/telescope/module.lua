7a867af35889d24b6c74ce4dcecaf49af5457c46
Ben Lubas <benmlubas@gmail.com>
2024-07-13 19:52:04 -0400

    fix: all the keybinds

--- lua/neorg/modules/core/integrations/telescope/module.lua
+++ lua/neorg/modules/core/integrations/telescope/module.lua
@@ -7,8 +7,29 @@ local neorg = require("neorg.core")
 local module = neorg.modules.create("core.integrations.telescope")
 
 module.setup = function()
-    return { success = true, requires = { "core.keybinds", "core.dirman" } }
+    return { success = true, requires = { "core.dirman" } }
 end
+
+-- To add a new picker:
+-- - Choose a name to add to this list (eg. <picker>)
+-- - Create a file in `lua/telescope/_extensions/neorg/<picker>.lua`
+-- - Add the picker to `lua/telescope/_extensions/neorg.lua`
+-- - Add it to the list in the README
+
+local pickers = {
+    "find_linkable",
+    "find_norg_files",
+    "insert_link",
+    "insert_file_link",
+    "search_headings",
+    "find_project_tasks",
+    "find_aof_project_tasks",
+    "find_aof_tasks",
+    "find_context_tasks",
+    "switch_workspace",
+    "backlinks.file_backlinks",
+    "backlinks.header_backlinks",
+}
 
 module.load = function()
     local telescope_loaded, telescope = pcall(require, "telescope")
@@ -17,20 +38,9 @@ module.load = function()
 
     telescope.load_extension("neorg")
 
-    module.required["core.keybinds"].register_keybinds(module.name, {
-        "find_linkable",
-        "find_norg_files",
-        "insert_link",
-        "insert_file_link",
-        "search_headings",
-        "find_project_tasks",
-        "find_aof_project_tasks",
-        "find_aof_tasks",
-        "find_context_tasks",
-        "switch_workspace",
-        "find_backlinks",
-        "find_header_backlinks",
-    })
+    for _, picker in ipairs(pickers) do
+        vim.keymap.set("", ("<Plug>(neorg.telescope.%s)"):format(picker), module.public[picker])
+    end
 end
 
 module.config.public = {
@@ -39,64 +49,10 @@ module.config.public = {
     },
 }
 
-module.public = {
-    find_linkable = require("telescope._extensions.neorg.find_linkable"),
-    find_norg_files = require("telescope._extensions.neorg.find_norg_files"),
-    insert_link = require("telescope._extensions.neorg.insert_link"),
-    insert_file_link = require("telescope._extensions.neorg.insert_file_link"),
-    search_headings = require("telescope._extensions.neorg.search_headings"),
-    find_project_tasks = require("telescope._extensions.neorg.find_project_tasks"),
-    find_context_tasks = require("telescope._extensions.neorg.find_context_tasks"),
-    find_aof_tasks = require("telescope._extensions.neorg.find_aof_tasks"),
-    find_aof_project_tasks = require("telescope._extensions.neorg.find_aof_project_tasks"),
-    switch_workspace = require("telescope._extensions.neorg.switch_workspace"),
-    find_backlinks = require("telescope._extensions.neorg.backlinks.file_backlinks"),
-    find_header_backlinks = require("telescope._extensions.neorg.backlinks.header_backlinks"),
-}
+module.public = {}
 
-module.on_event = function(event)
-    if event.split_type[2] == "core.integrations.telescope.find_linkable" then
-        module.public.find_linkable()
-    elseif event.split_type[2] == "core.integrations.telescope.find_norg_files" then
-        module.public.find_norg_files()
-    elseif event.split_type[2] == "core.integrations.telescope.insert_link" then
-        module.public.insert_link()
-    elseif event.split_type[2] == "core.integrations.telescope.insert_file_link" then
-        module.public.insert_file_link()
-    elseif event.split_type[2] == "core.integrations.telescope.search_headings" then
-        module.public.search_headings()
-    elseif event.split_type[2] == "core.integrations.telescope.find_project_tasks" then
-        module.public.find_project_tasks()
-    elseif event.split_type[2] == "core.integrations.telescope.find_aof_tasks" then
-        module.public.find_aof_tasks()
-    elseif event.split_type[2] == "core.integrations.telescope.find_aof_project_tasks" then
-        module.public.find_aof_project_tasks()
-    elseif event.split_type[2] == "core.integrations.telescope.find_context_tasks" then
-        module.public.find_context_tasks()
-    elseif event.split_type[2] == "core.integrations.telescope.switch_workspace" then
-        module.public.switch_workspace()
-    elseif event.split_type[2] == "core.integrations.telescope.find_backlinks" then
-        module.public.find_backlinks()
-    elseif event.split_type[2] == "core.integrations.telescope.find_header_backlinks" then
-        module.public.find_header_backlinks()
-    end
+for _, picker in ipairs(pickers) do
+    module.public[picker] = require(("telescope._extensions.neorg.%s"):format(picker))
 end
-
-module.events.subscribed = {
-    ["core.keybinds"] = {
-        ["core.integrations.telescope.find_linkable"] = true,
-        ["core.integrations.telescope.find_norg_files"] = true,
-        ["core.integrations.telescope.insert_link"] = true,
-        ["core.integrations.telescope.insert_file_link"] = true,
-        ["core.integrations.telescope.search_headings"] = true,
-        ["core.integrations.telescope.find_project_tasks"] = true,
-        ["core.integrations.telescope.find_context_tasks"] = true,
-        ["core.integrations.telescope.find_aof_tasks"] = true,
-        ["core.integrations.telescope.find_aof_project_tasks"] = true,
-        ["core.integrations.telescope.switch_workspace"] = true,
-        ["core.integrations.telescope.find_backlinks"] = true,
-        ["core.integrations.telescope.find_header_backlinks"] = true,
-    },
-}
 
 return module
--- lua/telescope/_extensions/neorg/insert_file_link.lua
+++ lua/telescope/_extensions/neorg/insert_file_link.lua
@@ -39,8 +39,7 @@ local function get_file_title(file)
         return nil
     end
 
-    local bufnr = dirman.get_file_bufnr(tostring(file))
-    local metadata = ts.get_document_metadata(bufnr)
+    local metadata = ts.get_document_metadata(file)
     if not metadata or not metadata.title then
         return nil
     end
